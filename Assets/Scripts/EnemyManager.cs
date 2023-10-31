7fa07eecc6276f5fc15212df329309dbc920584e
JackThomas1423 <Jthomas2780@student.pps.net>
2023-10-31 13:42:16 -0700

    did stuff

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    private GameObject player;
-    void Start()
+    public CircleCollider2D small_cl;
+    public CircleCollider2D big_cl;
+    private GameObject player = null;
+    private Animator _animator;
+
+    private void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        _animator = GetComponentInChildren<Animator>();
     }
-    
+
     void Update()
     {
-        Vector3 d = (transform.position - player.transform.position).normalized;
-        transform.position += -(d/1000);
+        if (player != null)
+        {
+            Vector3 d = (transform.position - player.transform.position).normalized;
+            transform.position += -(d / 850);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        player = other.gameObject;
+        small_cl.enabled = false;
+        big_cl.enabled = true;
+        _animator.enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        player = null;
+        small_cl.enabled = true;
+        big_cl.enabled = false;
+        _animator.enabled = false;
     }
 }
